98c7e71a8a06b2425dba8fd4d3bd2f276b3a7165
Rhian Rodrigues Barbosa <rhianbarbosa2003@gmail.com>
2024-05-18 20:52:25 -0300

    Password Hash

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using hotel_management.DAO;
 using hotel_management.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -10,13 +11,17 @@ using Microsoft.Extensions.WebEncoders.Testing;
 
 namespace hotel_management.Controllers
 {
-    public class LoginController : StandardController<StandardViewModel>
+    public class LoginController : StandardController<PersonViewModel>
     {
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error!");
+        }
+
+        public IActionResult Login(PersonViewModel model){
+            
         }
     }
 }
