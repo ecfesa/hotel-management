2026c787d8e1eb078d61abecd02eb167c0f6d414
Rhian Rodrigues Barbosa <rhianbarbosa2003@gmail.com>
2024-05-20 16:46:03 -0300

    Adjustments after roomcontroller->master merger

--- Program.cs
+++ Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Room}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
